bddb91a9a94a9873abce501e20d5950d00b493c4
Hsppie <wambimicheal@gmail.com>
2023-05-08 23:59:48 +0300

    merge conflict resolved

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-let storing = {};
-window.onload = (function () {
-  $("input[type=checkbox]").click(function () {
-    if(this.checked) {
-      storing[$(this).data('id')] = $(this).data('name');
-      $(".amenities h4").text(Object.values(storing).join(', '));
-    } else {
-      delete storing[$(this).data('id')];
-      if (Object.values(storing).length === 0) {
-	$(".amenities h4").html("&nbsp;");
-      }
-      else {
-	$(".amenities h4").text(Object.values(storing).join(', '));
-      }
-    }
-  });
-});
-=======
 $(document).ready(function(){
     let checkedAmenities = {};
     $("input[type=checkbox]").change(function(){
@@ -33,4 +14,5 @@ $(document).ready(function(){
           }
     });
 });
->>>>>>> secondary_hsppie
+
+ 
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-let storing = {};
-const url = 'http://0.0.0.0:5001/api/v1/status/';
-window.onload = (function () {
-  $("input[type=checkbox]").click(function () {
-    if(this.checked) {
-      storing[$(this).data('id')] = $(this).data('name');
-      $(".amenities h4").text(Object.values(storing).join(', '));
-    } else {
-      delete storing[$(this).data('id')];
-      if (Object.values(storing).length === 0) {
-	$('.amenities h4').html("&nbsp;");
-	} else {
-	  $(".amenities h4").text(Object.values(storing).join(', '));
-	  }
-      }
-  });
-  $.get(url, function (data, status) {
-    if (status === 'success') {
-      $('#api_status').addClass('available');
-    }
-  });
-});
-=======
 $(document).ready(function(){
     let checkedAmenities = {};
     $("input[type=checkbox]").change(function(){
@@ -46,4 +22,3 @@ $(document).ready(function(){
         }
     });
 });
->>>>>>> secondary_hsppie
